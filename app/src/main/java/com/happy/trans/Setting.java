09cdb7f1544f598a12d6a63326d4d4bece049ef7
gap97 <44805452+gap97@users.noreply.github.com>
2018-11-07 19:42:42 +0900

    Update Setting.java

--- app/src/main/java/com/happy/trans/Setting.java
+++ app/src/main/java/com/happy/trans/Setting.java
@@ -1,6 +1,8 @@
 package com.happy.trans;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,42 +13,56 @@ public class Setting extends AppCompatActivity {
 
     Button change_lang;
     Button change_disease;
+    Button change_emphasis;
+
+    TextView tv_change_lang;
+    TextView tv_change_disease;
+    TextView tv_change_emphasis;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
 
-        Intent intent=new Intent(this.getIntent());
-        final String lang=intent.getStringExtra("lang");
+        SharedPreferences prefs = getSharedPreferences("AppName", Context.MODE_PRIVATE);
+        String lang = prefs.getString("lang", "English");
 
         change_lang=(Button)findViewById(R.id.button_change_lang);
         change_disease=(Button)findViewById(R.id.button_change_disease);
+        change_emphasis=(Button)findViewById(R.id.button_change_emphasis);
+
+        tv_change_lang=(TextView) findViewById(R.id.change_lang);
+        tv_change_disease=(TextView)findViewById(R.id.change_disease);
+        tv_change_emphasis=(TextView)findViewById(R.id.change_emphasis);
+
         TextView tvSetting=(TextView)findViewById(R.id.tvSetting);
 
         if(lang.equalsIgnoreCase("English")) {
-            change_disease.setText("Set Highlight List");
-            change_lang.setText("Language change");
+            tv_change_disease.setText("Set Highlight List");
+            tv_change_lang.setText("Language change");
             tvSetting.setText("Setting");
+            tv_change_emphasis.setText("Highlighting format settings");
         }
 
         else if(lang.equalsIgnoreCase("Thai")) {
-            change_disease.setText("การตั้งค่ารายการรายชื่อ");
-            change_lang.setText("การเปลี่ยนภาษา");
+            tv_change_disease.setText("การตั้งค่ารายการรายชื่อ");
+            tv_change_lang.setText("การเปลี่ยนภาษา");
             tvSetting.setText("การตั้งราคา");
+            tv_change_emphasis.setText("การตั้งค่ารูปแบบการเน้นย้ำ");
         }
 
         else if(lang.equalsIgnoreCase("Viet")) {
-            change_disease.setText("Lập danh sách nhấn mạnh");
-            change_lang.setText("Thay đổi ngôn ngữ");
+            tv_change_disease.setText("Lập danh sách nhấn mạnh");
+            tv_change_lang.setText("Thay đổi ngôn ngữ");
             tvSetting.setText("Sự thiết lập");
+            tv_change_emphasis.setText("Thiết lập hình thức nhấn mạnh");
         }
 
         else {
-            change_disease.setText("강조 목록 설정");
-            change_lang.setText("언어변경");
-            tvSetting.setText("환경설정");
+            tv_change_disease.setText("设置强调目录");
+            tv_change_lang.setText("更改语言");
+            tvSetting.setText("配置环境");
+            tv_change_emphasis.setText("强调形式设置");
         }
-
 
         change_lang.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,7 +76,14 @@ public class Setting extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Setting.this, List.class);
-                intent.putExtra("lang",lang);
+                startActivity(intent);
+            }
+        });
+
+        change_emphasis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Setting.this, Change_Emphasis.class);
                 startActivity(intent);
             }
         });
